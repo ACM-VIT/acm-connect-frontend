deb42f7f6f3c850e174f6e74ab3e0229e9d8bd9f
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-28 02:47:44 +0530

    init app

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -25,14 +25,14 @@ const landing= () => {
     return (
         <header className={classes.root}>
             <AppBar position="static">
-                <Toolbar>
+                <Toolbar style={{background: '#128C7E'}}>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         ACM
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">Logout</Button>
                 </Toolbar>
             </AppBar>
         </header>
