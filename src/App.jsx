dd615c513cc4f4027d2cd10b4eb4729a8f9efdd2
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-27 23:37:00 +0530

    init app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Landing from "./components/landing/landing";
+
+const App = () => {
+  return (
+    <main className="App">
+      <Landing />
+      </main>
+  );
+}
+
+export default App;
+
