cc2bb1134a8d2d6909e344dc2ba3c5e1ab27f99e
Likhit Ajeesh <likhitajeesh20@gmail.com>
2021-07-30 05:07:21 +0400

    feat: Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './components/dashboard/dashboard';
+import Dashboard from './components/Dashboard/dashboard';
+import Footer from './components/Footer/footer';
 
 const App = () => {
   return (
     <div className="App">
       <Dashboard />
+      <Footer />
     </div>
   );
 };
